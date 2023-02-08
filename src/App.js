af7660c3a6353d07ea5bc3c119befd0524e9ebf4
Jon Beck <jonnydbeck@gmail.com>
2023-02-08 01:07:23 -0500

    Page Flipping Functionality Achived

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import React from "react";
 // Importing CSS
 import './App.css';
 // Importing Components
-import Header from "./components/Header";
+import FullPage from "./components/Navigation";
 
 // App is our top-level main component that references other components
-function App() {
-  return <Header />;
-}
+const App = () => <FullPage />;
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,38 @@
 import React from 'react';
 
-// Issue item accepts a single issue as a prop
-// This component is solely responsible for displaying the specific information for a given issue
-const Header = ({ issue }) => {
+function Header({ currentPage, handlePageChange }) {
   return (
     <div className="Header">
       <h1  className="Title">Jonathan Beck</h1>
       <div className='NavBar'>
-        <h2 className='NavItem'>About Me</h2>
-        <h2 className='NavItem'>Portfolio</h2>
-        <h2 className='NavItem'>Contact Me</h2>
-        <h2 className='NavItem'>Resume</h2>
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+        <h2 className='NavItem'>About Me</h2></a>
+
+        <a
+          href="#portfolio"
+          onClick={() => handlePageChange('Portfolio')}
+          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+        >
+        <h2 className='NavItem'>Portfolio</h2></a>
+
+        <a
+          href="#contact"
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+        <h2 className='NavItem'>Contact Me</h2></a>
+
+        <a
+          href="#resume"
+          onClick={() => handlePageChange('Resume')}
+          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+        >
+        <h2 className='NavItem'>Resume</h2></a>
+
       </div>
     </div>
   );
