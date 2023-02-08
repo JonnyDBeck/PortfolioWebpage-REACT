a051dc85756cb5b3e65427e4af3a214667961b46
Jon Beck <jonnydbeck@gmail.com>
2023-02-08 14:55:47 -0500

    Resume Page Completed

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import { Contact } from './pages/Contact';
-import Resume from './pages/Resume';
+import { Resume } from './pages/Resume';
 
 import Header from './Header';
 import Footer from './Footer';
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,9 +1,42 @@
 import React from 'react';
 
-export default function Resume() {
+export const Resume = () => {
+
+    const onButtonClick = () => {
+        fetch('JDB_Resume.pdf').then(response => {
+            response.blob().then(blob => {
+                const fileURL = window.URL.createObjectURL(blob);
+                let alink = document.createElement('a');
+                alink.href = fileURL;
+                alink.download = 'JDB_Resume.pdf';
+                alink.click();
+            })
+        })
+    }
+
   return (
-    <div className='theSection'>
-        Resume Page
+    <div className='theSection ResumeSection'>
+        <div className='ButtonContainer'>
+            <button className='ResumeButton'
+            onClick={onButtonClick}
+            >Download Resume PDF</button>            
+        </div>
+
+        <div>
+            <h1 className='Pros'>Proficiencies:</h1>
+                <ul>
+                    <li>JS / NodeJS</li>
+                    <li>SQL/ MySQL</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>MongoDB</li>
+                    <li>MERN STack</li>
+                    <li>Python</li>
+                    <li>C#, C++, C</li>
+                    <li>React</li>
+                    <li>+ Many More Skills</li>
+                </ul>
+        </div>
     </div>
   );
 }
