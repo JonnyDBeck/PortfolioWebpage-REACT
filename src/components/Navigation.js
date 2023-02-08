8a637648efa0d4a261dd99c39dfc40298d269f34
Jon Beck <jonnydbeck@gmail.com>
2023-02-08 14:15:44 -0500

    Contact Page Made

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
+import { Contact } from './pages/Contact';
 import Resume from './pages/Resume';
 
 import Header from './Header';
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
-export default function Contact() {
+export const Contact = () => {
+
+    const [buttonDisabled, setButtonDisabled] = React.useState(false);
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+      setButtonDisabled(true);
+  
+      emailjs.sendForm('service_csxgf0k', 'template_xcz76ok', form.current, 'COOkbhwTpWBujQEZU')
+        .then((result) => {
+            console.log(result.text);
+            alert("Email Sent")
+        }, (error) => {
+            console.log(error.text);
+              alert(error.text)
+        });
+
+    };
+
   return (
-    <div>
-
+    <div className='theSection'>
+        <form ref={form} onSubmit={sendEmail}>
+            <label for="name">Name:</label>
+            <input className='inputBoxes' type="text" id="name" name="name" /><br />
+            <label for="email">Email Adress:</label>
+            <input className='inputBoxes' type="email" id="email" name="email" /><br />
+            <label for="textBox">Email Text:</label><br />
+            <textarea id="textBox" name="message">
+                Hello Jonathan Beck,
+            </textarea><br />
+            <p></p>
+            <input className='Submit' type="submit" value="Send" 
+            disabled={buttonDisabled} />
+        </form>
     </div>
   );
 }
